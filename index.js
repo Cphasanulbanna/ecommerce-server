62a9e39a87887bddc1bc0e4ef0767cb7770822ea
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-12 22:13:53 +0530

    created user model

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+const { connectDb } = require("./config/db");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true }));
+
+connectDb();
 
 app.listen(PORT, () => console.log(`Server is running on port:${[PORT]}`));
---
+++ models/userModel.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: [true, "Please enter your full name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your full email"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your full password"],
+        minLenght: [4, "Password should be greater than 4 characters"],
+        select: false,
+    },
+    phoneNumber: {
+        type: Number,
+    },
+    address: [
+        {
+            country: {
+                type: String,
+            },
+            city: {
+                type: String,
+            },
+            address1: {
+                type: String,
+            },
+            address2: {
+                type: String,
+            },
+            zipCode: {
+                type: Number,
+            },
+            addressType: {
+                type: String,
+            },
+        },
+    ],
+    role: {
+        type: String,
+        default: "user",
+    },
+    profilePic: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    resetPasswordToken: String,
+    resetPasswordTime: Date,
+});
+
+module.exports = mongoose.model("User", userSchema);
