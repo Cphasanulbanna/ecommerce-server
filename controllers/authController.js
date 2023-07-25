d835a91f1c5e13389e0e7d5423342a260748de61
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-25 19:24:48 +0530

    added logout api

--- controllers/authController.js
+++ controllers/authController.js
@@ -145,3 +145,17 @@ exports.login = async (req, res) => {
         res.status(500).json({ message: "server error", success: false });
     }
 };
+
+exports.logout = async (req, res) => {
+    try {
+        res.cookie("refreshToken", null, {
+            expires: new Date(Date.now()),
+            httpOnly: true,
+        });
+
+        res.status(200).json({ success: true, message: "Logout successful" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "server error", success: false });
+    }
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, activateAccount, login } = require("../controllers/authController");
+const { signup, activateAccount, login, logout } = require("../controllers/authController");
 const { upload } = require("../middlewares/upload");
 
 router.post("/signup", upload.single("profile_pic"), signup);
 router.post("/activation", activateAccount);
 router.post("/login", login);
+router.get("/", logout);
 
 module.exports = router;
